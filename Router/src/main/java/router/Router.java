7d9f35462d8d7ece826120d2b69a85fd352ba698
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2020-10-05 15:17:15 +0200

    Project setup done

---
+++ Router/src/main/java/router/Router.java
@@ -0,0 +1,12 @@
+package router;
+
+/**
+ * Hello world!
+ *
+ */
+public class Router {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
